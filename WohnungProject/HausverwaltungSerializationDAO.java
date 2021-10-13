4c269d88705072a84557646f387af6154993b541
gabrielh00 <a11914741@univie.ac.at>
2021-10-13 18:04:40 +0200

    Serialization

--- WohnungProject/HausverwaltungSerializationDAO.java
+++ WohnungProject/HausverwaltungSerializationDAO.java
@@ -1,9 +1,33 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class HausverwaltungSerializationDAO implements HausverwaltungDAO{
+public class HausverwaltungSerializationDAO<T> implements HausverwaltungDAO{
     private List<Wohnung> wohnungList;
+    private String dateiName;
+
+    public HausverwaltungSerializationDAO(List<Wohnung> wohnungList, String dateiName) {
+        this.setWohnungList(wohnungList);
+        this.setDateiName(dateiName);
+    }
+
+    public <T> void serializeObject(T object){
+        try {
+            ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(dateiName));
+            stream.writeObject(object);
+            stream.close();
+        } catch (IOException ioex) {
+            System.err.println("Fehler bei Serialisierung: " + ioex.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public <T> void deserializeObject(T object) {
+
+    }
 
     @Override
     public List<Wohnung> getWohnungen() {
@@ -38,5 +62,16 @@ public class HausverwaltungSerializationDAO implements HausverwaltungDAO{
             throw new IllegalArgumentException("Error: Wohnung nicht vorhanden. (id=" + idForDeletion + ")");
         }
         this.wohnungList.remove(tempWohn);
+    }
+
+    public void setWohnungList(List<Wohnung> wohnungList) {
+        this.wohnungList = wohnungList;
+    }
+
+    public void setDateiName(String dateiName) {
+        if(dateiName.isEmpty()){
+            throw new IllegalArgumentException("Error: Parameter ungueltig.");
+        }
+        this.dateiName = dateiName;
     }
 }
